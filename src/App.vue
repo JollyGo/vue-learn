d16a742c7beb92f88045f6fb1c9f5d3c581cccbb
JollyGo <wjl_0017@163.com>
2020-03-11 00:35:04 +0800

    Antd 错误

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <a-menu
+      :defaultSelectedKeys="['1']"
+      :defaultOpenKeys="['2']"
+      mode="inline"
+      theme="dark"
+      :inlineCollapsed="collapsed"
+    >
+      <template v-for="item in list">
+        <a-menu-item v-if="!item.children" :key="item.key">
+          <a-icon type="pie-chart" />
+          <span>{{item.title}}</span>
+        </a-menu-item>
+        <sub-menu v-else :menu-info="item" :key="item.key" />
+      </template>
+    </a-menu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import SubMenu from "./components/SubMenu.vue";
+import moment from "moment";
+import { version, Menu, Icon } from "ant-design-vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    AMenu: Menu,
+    AIcon: Icon,
+    AMenuItem: Menu.Item,
+    // HelloWorld
+    SubMenu
+  },
+  data() {
+    return {
+      moment,
+      version,
+      collapsed: false,
+      list: [
+        {
+          key: "1",
+          title: "Option 1"
+        },
+        {
+          key: "2",
+          title: "Navigation 2",
+          children: [
+            {
+              key: "2.1",
+              title: "Navigation 3",
+              children: [{ key: "2.1.1", title: "Option 2.1.1" }]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    }
   }
-}
+};
 </script>
 
 <style>
